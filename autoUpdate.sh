58fdb1f20279826d30d0eec2596b13cfdda2cdc0
marktsao <tsaohungbo1206@icloud.com>
2021-03-10 18:32:38 +0800

    auto update 20210310 183238

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,5 +1,5 @@
 timestamp=$(date +'%Y%m%d %H%M%S')
 echo $timestamp
-git add -A
-git commit -m "auto update $timestamp"
-git push origin master
+/usr/bin/git add -A
+/usr/bin/git commit -m "auto update $timestamp"
+/usr/bin/git push origin master
