ad7b5fd7fbbcb6f9e18970d4c62c1b7b815734ed
marktsao <tsaohungbo1206@icloud.com>
2021-03-10 19:01:20 +0800

    20210310 auto update

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -6,13 +6,13 @@ time=$(date +'%H%M%S')
 echo "$date $time"
 
 # Download the latest csv of stock price
-python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/Taiwan_Stocks/update_taiwan_stock.py
+#python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/Taiwan_Stocks/update_taiwan_stock.py
 
 # Make prediction
-python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/all_stock_4value_together.pyi
+#python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/all_stock_4value_together.pyi
 
 # Copy TOMORROW_PRICE.csv to /home/marktsao/stock_website
-cp /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/TOMORROW_PRICE.csv /home/marktsao/stock_website/TOMORROW_PRICE.csv
+cp /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/TOMORROW_PRICE.csv /home/marktsao/stock_website
 
 # Push to github 
 cd /home/marktsao/stock_website/
