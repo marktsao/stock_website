83a9348a897d8af5a62a85c160d8103a4d0d26b6
marktsao <tsaohungbo1206@icloud.com>
2021-03-10 20:08:37 +0800

    20210310 auto update

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
-
 # Get current date & time
 date=$(date +'%Y%m%d')
 time=$(date +'%H%M%S')
 echo "$date $time"
 
+conda activate myenv
+
 # Download the latest csv of stock price
 python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/Taiwan_Stocks/update_taiwan_stock.py
 
 # Make prediction
-python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/all_stock_4value_together.pyi
+python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/all_stock_4value_together.py
 
 # Copy TOMORROW_PRICE.csv to /home/marktsao/stock_website
 cp /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/TOMORROW_PRICE.csv /home/marktsao/stock_website
@@ -19,3 +19,5 @@ cd /home/marktsao/stock_website/
 /usr/bin/git add -A
 /usr/bin/git commit -m "$date auto update"
 /usr/bin/git push origin master
+
+curl -X POST -H "Authorization: Bearer gZd0yMvonRUdlrR9LSquHCIKGGau7hjjADlzLxOmJ3W" -d "$date" https://notify-api.line.me/api/notify
