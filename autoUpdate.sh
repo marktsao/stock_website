062a7762d278532bf57283b5f6e18991b37a8cc3
marktsao <tsaohungbo1206@icloud.com>
2021-03-11 16:27:07 +0800

    20210311 auto update

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -5,16 +5,18 @@ date=$(date +'%Y%m%d')
 time=$(date +'%H%M%S')
 echo "$date $time"
 
-# curl -X POST -H "Authorization: Bearer gZd0yMvonRUdlrR9LSquHCIKGGau7hjjADlzLxOmJ3W" -d "message=$date 訓練開始" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer gZd0yMvonRUdlrR9LSquHCIKGGau7hjjADlzLxOmJ3W" -d "message=$date 訓練開始" https://notify-api.line.me/api/notify
 
 source /home/marktsao/anaconda3/etc/profile.d/conda.sh
 conda activate myenv
 
 # Download the latest csv of stock price
-python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/Taiwan_Stocks/update_taiwan_stock.py
+cd /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/Taiwan_Stocks
+python update_taiwan_stock.py
 
 # Make prediction
-# python /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/all_stock_4value_together.py
+cd /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM
+python all_stock_4value_together.py
 
 # Copy TOMORROW_PRICE.csv to /home/marktsao/stock_website
 cp /home/marktsao/DLpractice/Stock_Prices_Prediction_LSTM/TOMORROW_PRICE.csv /home/marktsao/stock_website
@@ -25,5 +27,5 @@ cd /home/marktsao/stock_website/
 /usr/bin/git commit -m "$date auto update"
 /usr/bin/git push origin master
 
-# curl -X POST -H "Authorization: Bearer gZd0yMvonRUdlrR9LSquHCIKGGau7hjjADlzLxOmJ3W" -d "message=$date 訓練完成
+curl -X POST -H "Authorization: Bearer gZd0yMvonRUdlrR9LSquHCIKGGau7hjjADlzLxOmJ3W" -d "message=$date 訓練完成
 # 查看結果：https://marktsao.github.io/stock_website/" https://notify-api.line.me/api/notify
