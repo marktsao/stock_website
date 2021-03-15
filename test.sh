10495861438f427189a5a23a4481c4e49a5258b2
marktsao <tsaohungbo1206@icloud.com>
2021-03-15 16:37:30 +0800

    20210315 auto update

---
+++ test.sh
@@ -0,0 +1,10 @@
+token=""
+
+if [$1=="private"]
+then
+    token="gZd0yMvonRUdlrR9LSquHCIKGGau7hjjADlzLxOmJ3W" # private
+else
+    token="Oh4jBjR5YQAvw2GUmAZByamh25dKIgY3HMshDACzhLr" # public
+fi
+
+echo $token
